db08da67a7d6d8c8f5c40be8131d9b37d9ca8ba4
Johnatan Dias <johnatan.dias@gmail.com>
2020-09-17 20:43:36 -0300

    i3: update fix monitor script

--- i3/fix-monitor.sh
+++ i3/fix-monitor.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-xrandr --output HDMI-1 --primary --above eDP-1
+xrandr \
+  --output eDP1 \
+  --mode 1366x768 \
+  --pos 224x1080 \
+  --rotate normal \
+  --output HDMI1 \
+    --primary \
+    --mode 1920x1080 \
+    --pos 0x0 \
+    --rotate normal \
+    --output VIRTUAL1 --off
